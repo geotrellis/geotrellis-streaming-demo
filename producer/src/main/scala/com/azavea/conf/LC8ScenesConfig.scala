38f95ff43cc98730cd31ebeaae67bdda7c34d054
Grigory Pomadchin <gr.pomadchin@gmail.com>
2019-04-21 13:11:09 -0400

    Fix the codestyle

--- producer/src/main/scala/com/azavea/conf/LC8ScenesConfig.scala
+++ producer/src/main/scala/com/azavea/conf/LC8ScenesConfig.scala
@@ -23,7 +23,7 @@ case class LC8Scene(name: String, band: Int, count: Int, crs: String, outputPath
   private val pattern = """_([0-9]{6})_""".r
   def path: String = {
     val (path, row) = pattern.findFirstIn(name).getOrElse(throw new Exception("Bad LC8 Scene name")).init.tail.splitAt(3)
-    s"s3://landsat-pds/c1/L8/${path}/${row}/$name/${name}_B${band}.TIF"
+    s"s3://landsat-pds/c1/L8/${path}/${row}/${name}/${name}_B${band}.TIF"
   }
 
   def getCRS: CRS = Try(CRS.fromName(crs)) getOrElse CRS.fromString(crs)
